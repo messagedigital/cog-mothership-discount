a7bcc992d20f2e4637006e50308368a59bd7df1f
Joe Holdcroft <joe@message.co.uk>
2014-03-05 16:56:32 +0000

    Updating service definitions for Pimple 2

--- src/Bootstrap/Services.php
+++ src/Bootstrap/Services.php
@@ -9,29 +9,28 @@ class Services implements ServicesInterface
 {
 	public function registerServices($services)
 	{
-
-		$services['discount.loader'] = function($c) {
+		$services['discount.loader'] = $services->factory(function($c) {
 			return new Discount\Discount\Loader($c['db.query'], $c['product.loader']);
-		};
+		});
 
-		$services['discount.create'] = function($c) {
+		$services['discount.create'] = $services->factory(function($c) {
 			return new Discount\Discount\Create($c['db.query'], $c['user.current']);
-		};
+		});
 
-		$services['discount.edit'] = function($c) {
+		$services['discount.edit'] = $services->factory(function($c) {
 			return new Discount\Discount\Edit($c['db.transaction'], $c['user.current']);
-		};
+		});
 
-		$services['discount.delete'] = function($c) {
+		$services['discount.delete'] = $services->factory(function($c) {
 			return new Discount\Discount\Delete($c['db.query'], $c['user.current']);
-		};
+		});
 
-		$services['discount.validator'] = function($c) {
+		$services['discount.validator'] = $services->factory(function($c) {
 			return new Discount\Discount\Validator($c['discount.loader'], $c['discount.order-discount-factory']);
-		};
+		});
 
-		$services['discount.order-discount-factory'] = function($c) {
+		$services['discount.order-discount-factory'] = $services->factory(function($c) {
 			return new Discount\Discount\OrderDiscountFactory();
-		};
+		});
 	}
 }
