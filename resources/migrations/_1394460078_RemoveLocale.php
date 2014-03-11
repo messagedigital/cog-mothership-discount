f722bc8be5a5d694cacdf90044522167106a4438
Iris Schaffer <irisSchaffer@gmx.net>
2014-03-11 13:36:11 +0000

    Updating Migration

--- resources/migrations/_1394460078_RemoveLocale.php
+++ resources/migrations/_1394460078_RemoveLocale.php
@@ -6,32 +6,24 @@ class _1394460078_RemoveLocale extends Migration
 {
 	public function up()
 	{
-		$this->run('
-			ALTER TABLE `discount_amount` DROP PRIMARY KEY;
-			ALTER TABLE `discount_amount` PRIMARY KEY (`discount_id`,`currency_id`);
-			ALTER TABLE `discount_amount` DROP `locale`;
-		');
+		$this->run('ALTER TABLE `discount_amount` DROP PRIMARY KEY;');
+		$this->run('ALTER TABLE `discount_amount` ADD PRIMARY KEY (`discount_id`,`currency_id`);');
+		$this->run('ALTER TABLE `discount_amount` DROP `locale`;');
 
-		$this->run('
-			ALTER TABLE `discount_threshold` DROP PRIMARY KEY;
-			ALTER TABLE `discount_threshold` PRIMARY KEY (`discount_id`,`currency_id`);
-			ALTER TABLE `discount_threshold` DROP `locale`;
-		');
+		$this->run('ALTER TABLE `discount_threshold` DROP PRIMARY KEY;');
+		$this->run('ALTER TABLE `discount_threshold` ADD PRIMARY KEY (`discount_id`,`currency_id`);');
+		$this->run('ALTER TABLE `discount_threshold` DROP `locale`;');
 	}
 
 	public function down()
 	{
-		$this->run('
-			ALTER TABLE `discount_amount` ADD `locale` varchar(50) NOT NULL;
-			ALTER TABLE `discount_amount` DROP PRIMARY KEY;
-			ALTER TABLE `discount_amount` PRIMARY KEY (`discount_id`,`currency_id`,`locale`);
-		');
+		$this->run('ALTER TABLE `discount_amount` ADD `locale` varchar(50) NOT NULL;');
+		$this->run('ALTER TABLE `discount_amount` DROP PRIMARY KEY;');
+		$this->run('ALTER TABLE `discount_amount` ADD PRIMARY KEY (`discount_id`,`currency_id`,`locale`);');
 
-		$this->run('
-			ALTER TABLE `discount_threshold` ADD `locale` varchar(50) NOT NULL;
-			ALTER TABLE `discount_threshold` DROP PRIMARY KEY;
-			ALTER TABLE `discount_threshold` PRIMARY KEY (`discount_id`,`currency_id`,`locale`);
-		');
+		$this->run('ALTER TABLE `discount_threshold` ADD `locale` varchar(50) NOT NULL;');
+		$this->run('ALTER TABLE `discount_threshold` DROP PRIMARY KEY;');
+		$this->run('ALTER TABLE `discount_threshold` ADD PRIMARY KEY (`discount_id`,`currency_id`,`locale`);');
 
 	}
 }
