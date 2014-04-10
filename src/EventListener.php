7f60b88612fe605ccec17dc9964779d1b2a4ec0d
Laurence Roberts <lsjroberts@gmail.com>
2014-04-10 10:55:10 +0100

    Added dashboard modules

---
+++ src/EventListener.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Message\Mothership\Commerce;
+
+use Message\Cog\Event\SubscriberInterface;
+use Message\Cog\Event\EventListener as BaseListener;
+
+use Message\Mothership\ControlPanel\Event\Dashboard\DashboardEvent;
+
+/**
+ * Event listener for core Mothership Discount functionality.
+ *
+ * @author Laurence Roberts <laurence@message.co.uk>
+ */
+class EventListener extends BaseListener implements SubscriberInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	static public function getSubscribedEvents()
+	{
+		return array(
+			DashboardEvent::DASHBOARD_INDEX => array(
+				'buildDashboardIndex'
+			),
+			'dashboard.commerce.discounts' => array(
+				'buildDashboardDiscounts',
+			),
+		);
+	}
+
+	/**
+	 * Add controller references to the dashboard index.
+	 *
+	 * @param  DashboardEvent $event
+	 */
+	public function buildDashboardIndex(DashboardEvent $event)
+	{
+		$event->addReference('Message:Mothership:Commerce::Controller:Module:Dashboard:DiscountRevenue#index');
+	}
+
+	/**
+	 * Add controller references to the discounts dashboard.
+	 *
+	 * @param  DashboardEvent $event
+	 */
+	public function buildDashboardDiscounts(DashboardEvent $event)
+	{
+		$event->addReference('Message:Mothership:Discount::Controller:Module:Dashboard:DiscountRevenue#index');
+	}
+}
